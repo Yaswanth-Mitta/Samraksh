f8ed0b74dc3f340b455b218c9633643d4a72076e
Yaswanth Mitta <yaswanthmitta@gmail.com>
2023-09-10 12:56:40 +0530

    Connected to Firebase using CLI Tools

--- rakshak_my_app/lib/main.dart
+++ rakshak_my_app/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rakshak_my_app/auth/login_screen.dart';
+import 'package:rakshak_my_app/firebase_options.dart';
 
-void main() {
+// void main() {
+//   runApp(const MyApp());
+// }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
